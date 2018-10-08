211e0a4494806c21da92d2123895b4451d5d673b
zhouchao <810753835@qq.com>
2018-10-08 18:35:30 +0800

    排序

---
+++ app/src/main/java/origin/com/libraryhttp/http/simple/algorithm/AlgorithmJava.java
@@ -0,0 +1,36 @@
+package origin.com.libraryhttp.http.simple.algorithm;
+
+/**
+ * 算法学习
+ * Created by zc on 2018/10/8
+ */
+public class AlgorithmJava {
+
+    /**
+     * 1.快速排序 <br/>
+     *  快速排序（QuickSort）是对冒泡排序的一种改进。 <br/>
+     *  基本思想是：通过一趟排序将要排序的数据分割成独立的两个部分。 <br/>
+     *  其中一部分的所有数据都比另外一部分的所有数据都要小， <br/>
+     *  然后再按此方法对这两部分数据分别进行快速排序 <br/>
+     *  整个过程可以递归进行， <br/>
+     *  博客介绍 ：http://developer.51cto.com/art/201403/430986.htm <br/>
+     */
+    private void quickSort(int left ,int right){
+
+    }
+
+    /**
+     * 2.冒泡排序(BubbleSort)<br/>
+     *   bubbleSort 是一种计算机科学领域比较简单的排序算法 <br/>
+     *   原理：每次比较相邻的元素，错误交换位置 <br/>
+     *   博客介绍：http://blog.51cto.com/ahalei/1364401 <br/>
+     */
+    private void bubbleSort(){
+
+    }
+    /**
+     * 队列 博客介绍：http://blog.51cto.com/ahalei/1371613 <br/>
+     * 栈 博客介绍:http://blog.51cto.com/ahalei/1377872 <br/>
+     */
+
+}
