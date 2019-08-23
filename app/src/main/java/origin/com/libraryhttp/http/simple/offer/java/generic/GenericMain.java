ae821557f0c09f46d084768b93b28cfc08eded5c
zhouchao <810753835@qq.com>
2019-08-23 18:26:57 +0800

    添加注解

--- app/src/main/java/origin/com/libraryhttp/http/simple/offer/java/generic/GenericMain.java
+++ app/src/main/java/origin/com/libraryhttp/http/simple/offer/java/generic/GenericMain.java
@@ -1,10 +1,38 @@
 package origin.com.libraryhttp.http.simple.offer.java.generic;
 
+import java.util.ArrayList;
+
 /**
- * 泛型整理
  * Created by zc on 2019/6/11
  */
 public class GenericMain {
 
+// https://blog.csdn.net/jeffleo/article/details/52250948
+//    https://blog.csdn.net/yangguanghaozi/article/details/54632477
+    /**
+     * 理解：
+     * 上界 <? extend Fruit>
+     * 下界 <? super Apple>
+     *
+     */
+//    上界 <? extend Fruit> ，表示所有继承Fruit的子类，但是具体是哪个子类，无法确定，
+//    所以调用add的时候，要add什么类型，谁也不知道。
+//    但是get的时候，不管是什么子类，不管追溯多少辈，肯定有个父类是Fruit，
+//    所以，我都可以用最大的父类Fruit接着，也就是把所有的子类向上转型为Fruit。
+//
+//    下界 <? super Apple>，表示Apple的所有父类，包括Fruit，一直可以追溯到老祖宗Object 。
+//    那么当我add的时候，我不能add Apple的父类，因为不能确定List里面存放的到底是哪个父类。
+//    但是我可以add Apple及其子类。因为不管我的子类是什么类型，
+//    它都可以向上转型为Apple及其所有的父类甚至转型为Object 。
+//    但是当我get的时候，Apple的父类这么多，我用什么接着呢，除了Object，其他的都接不住。
 
+//2.上界和下界的特点
+//    上界的list只能get，不能add（确切地说不能add出除null之外的对象，包括Object）
+//    下界的list只能add，不能get
+
+//3. 理解擦除
+//    Class a = new ArrayList<Integer>().getClass();
+//    Class b = new ArrayList<String>().getClass();
+//
+//    System.out.println(a == b);//输出结果true
 }
--- app/src/main/java/origin/com/libraryhttp/http/simple/offer/java/generic/code/PairTest3.java
+++ app/src/main/java/origin/com/libraryhttp/http/simple/offer/java/generic/code/PairTest3.java
@@ -1,87 +1,80 @@
 package origin.com.libraryhttp.http.simple.offer.java.generic.code;
 
 /**
- * @version 1.01 2012-01-26
  * @author Cay Horstmann
+ * @version 1.01 2012-01-26
  */
-public class PairTest3
-{
-   public static void main(String[] args)
-   {
-      Manager ceo = new Manager("Gus Greedy", 800000, 2003, 12, 15);
-      Manager cfo = new Manager("Sid Sneaky", 600000, 2003, 12, 15);
+public class PairTest3 {
+    public static void main(String[] args) {
+        Manager ceo = new Manager("Gus Greedy", 800000, 2003, 12, 15);
+        Manager cfo = new Manager("Sid Sneaky", 600000, 2003, 12, 15);
 
-      ceo.setBonus(1000000);
-      cfo.setBonus(500000);
+        ceo.setBonus(1000000);
+        cfo.setBonus(500000);
 
-      //？ 和 extends 的理解 Employee  是Manager的父类
-      Pair<Manager> buddies = new Pair<>(ceo, cfo);
-      printBuddies(buddies);
+        //？ 和 extends 的理解 Employee  是Manager的父类
+        Pair<Manager> buddies = new Pair<>(ceo, cfo);
+        printBuddies(buddies);
 
 
-      //？ 和 super 的理解 Employee  是Manager的父类
-      Pair<Employee> result = new Pair<>();
-      Manager[] managers = { ceo, cfo };
-      minmaxBonus(managers, result);
+        //？ 和 super 的理解 Employee  是Manager的父类
+        Pair<Employee> result = new Pair<>();
+        Manager[] managers = {ceo, cfo};
+        minmaxBonus(managers, result);
 
-      //总结：
-      // extends 可以get 但是set不可以
-      // super 可以set 但是不可以get
-
-
-      System.out.println("first: " + result.getFirst().getName() 
-         + ", second: " + result.getSecond().getName());
-      maxminBonus(managers, result);
-      System.out.println("first: " + result.getFirst().getName() 
-         + ", second: " + result.getSecond().getName());
-   }
-
-   public static void printBuddies(Pair<? extends Employee> p)
-   {
-      Employee first = p.getFirst();
-      Employee second = p.getSecond();
-
-      System.out.println(first.getName() + " and " + second.getName() + " are buddies.");
-   }
-
-   public static void minmaxBonus(Manager[] a, Pair<? super Manager> result)
-   {
-      if (a.length == 0) return;
-      Manager min = a[0];
-      Manager max = a[0];
-      for (int i = 1; i < a.length; i++)
-      {
-         if (min.getBonus() > a[i].getBonus()) min = a[i];
-         if (max.getBonus() < a[i].getBonus()) max = a[i];
-      }
-      result.setFirst(min);
-      result.setSecond(max);
+        //总结：
+        // extends 可以get 但是set不可以
+        // super 可以set 但是不可以get
 
 
-   }
+        System.out.println("first: " + result.getFirst().getName()
+                + ", second: " + result.getSecond().getName());
+        maxminBonus(managers, result);
+        System.out.println("first: " + result.getFirst().getName()
+                + ", second: " + result.getSecond().getName());
+    }
 
-   public static void maxminBonus(Manager[] a, Pair<? super Manager> result)
-   {
-      minmaxBonus(a, result);
-      PairAlg.swapHelper(result); // OK--swapHelper captures wildcard type
-   }
+    public static void printBuddies(Pair<? extends Employee> p) {
+        Employee first = p.getFirst();
+        Employee second = p.getSecond();
+
+        System.out.println(first.getName() + " and " + second.getName() + " are buddies.");
+    }
+
+    public static void minmaxBonus(Manager[] a, Pair<? super Manager> result) {
+        if (a.length == 0) return;
+        Manager min = a[0];
+        Manager max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.getBonus() > a[i].getBonus()) min = a[i];
+            if (max.getBonus() < a[i].getBonus()) max = a[i];
+        }
+        result.setFirst(min);
+        result.setSecond(max);
+
+
+    }
+
+    public static void maxminBonus(Manager[] a, Pair<? super Manager> result) {
+        minmaxBonus(a, result);
+        PairAlg.swapHelper(result); // OK--swapHelper captures wildcard type
+    }
 }
 
-class PairAlg
-{
-   public static boolean hasNulls(Pair<?> p)
-   {
-      return p.getFirst() == null || p.getSecond() == null;
-   }
+class PairAlg {
+    public static boolean hasNulls(Pair<?> p) {
+        return p.getFirst() == null || p.getSecond() == null;
+    }
 
-   public static void swap(Pair<?> p) { swapHelper(p); }
+    public static void swap(Pair<?> p) {
+        swapHelper(p);
+    }
 
-   public static <T> void swapHelper(Pair<T> p)
-   {
-      T t = p.getFirst();
-      p.setFirst(p.getSecond());
-      p.setSecond(t);
-   }
+    public static <T> void swapHelper(Pair<T> p) {
+        T t = p.getFirst();
+        p.setFirst(p.getSecond());
+        p.setSecond(t);
+    }
 }
 
 
